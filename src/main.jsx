69bb84097d85168eceba887506aee2e348830959
Ahmed123598 <skshahbaz783@gmail.com>
2025-07-16 13:44:07 +0530

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,28 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.jsx'
 import Aboutus from './Aboutus.jsx';
 import Courses from './Courses.jsx';
-import Registrationform from './RegistrationForm.jsx';
+import RegistrationForm from './RegistrationForm.jsx'; // ✅ Fix: uppercase F
 import Home from './Home.jsx';
 import Contactus from './Contactus.jsx';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
-   
-   
-   
-    <Routes>
-
-      <Route path="/" element={<App/>} />
-      
-      <Route path="/about" element={<Aboutus/>} />
-      <Route path="/course" element={<Courses />} />
-      <Route path="/registrationform" element={<Registrationform />} />
-     <Route path='/contactus' element={<Contactus/>}/>
-  
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<Aboutus />} />
+        <Route path="/course" element={<Courses />} />
+        <Route path="/registrationform" element={<RegistrationForm />} /> {/* ✅ Fix */}
+        <Route path='/contactus' element={<Contactus />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-
-  ) 
+)
