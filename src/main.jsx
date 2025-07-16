7b433c641c22e812ca398cd5f76034090fc41035
Ahmed123598 <skshahbaz783@gmail.com>
2025-07-16 14:02:13 +0530

    over

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.jsx'
 import Aboutus from './Aboutus.jsx';
 import Courses from './Courses.jsx';
-import RegistrationForm from './RegistrationForm.jsx'; // ✅ Fix: uppercase F
+import Registrationform from './Registrationform.jsx'
 import Home from './Home.jsx';
 import Contactus from './Contactus.jsx';
 
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/about" element={<Aboutus />} />
         <Route path="/course" element={<Courses />} />
-        <Route path="/registrationform" element={<RegistrationForm />} /> {/* ✅ Fix */}
+        <Route path="/registrationform" element={<Registrationform />} /> 
         <Route path='/contactus' element={<Contactus />} />
       </Routes>
     </BrowserRouter>
